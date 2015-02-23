43d98e65c1bb01dd7e8eab03231e3ebf5b67d076
Gokce Mutlu <gokce.mutlu@tomtom.com>
2015-02-23 10:24:10 +0100

    fixing the error logging

--- app/src/main/java/com/gokcemutlu/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/gokcemutlu/example/sunshine/ForecastFragment.java
@@ -57,8 +57,10 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
-    private class FetchWeatherTask extends AsyncTask<Void, Void, Void>
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>
     {
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
         @Override
         protected Void doInBackground(Void... params) {
             // These two need to be declared outside the try/catch
@@ -103,7 +105,7 @@ public class ForecastFragment extends Fragment {
                 }
                 forecastJsonStr = buffer.toString();
             } catch (IOException e) {
-                Log.e("PlaceholderFragment", "Error ", e);
+                Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
                 // to parse it.
                 return null;
@@ -115,7 +117,7 @@ public class ForecastFragment extends Fragment {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
                 }
             }
