4a20e331d3580507956279bbc7a722663f68c752
Gokce Mutlu <gokce.mutlu@tomtom.com>
2015-02-26 11:39:14 +0100

    remove refresh menu

--- app/src/main/java/com/gokcemutlu/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/gokcemutlu/example/sunshine/ForecastFragment.java
@@ -10,9 +10,6 @@ import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -82,27 +79,6 @@ public class ForecastFragment extends Fragment {
         super.onCreate(savedInstanceState);
         // Add this line in order for this fragment to handle menu events.
         setHasOptionsMenu(true);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_forecast, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_refresh) {
-            updateWeather();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void updateWeather() {
